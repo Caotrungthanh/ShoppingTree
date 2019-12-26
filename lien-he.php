dd21253f9ba9f65be1f0c48fe7a00a32d991ed39
Cao Trung Thành <37401313+Caotrungthanh@users.noreply.github.com>
2019-12-26 22:40:39 +0700

    Cập nhập form liên hệ

--- lien-he.php
+++ lien-he.php
@@ -3,5 +3,21 @@
 ?>
 
 <?php require_once __DIR__. "/layouts/header.php";  ?>
-
+    <div class="contact">
+        <form method="post" action="">
+            <div class="form-group">
+                <label for="my-input">Text</label>
+                <input id="my-input" class="form-control" type="text" name="tennguoigui">
+            </div>
+            <div class="form-group">
+                <label for="my-input">Text</label>
+                <input id="my-input" class="form-control" type="text" name="email">
+            </div>
+            <div class="form-group">
+                <label for="my-input">Text</label>
+                <textarea name="phanhoi" id="" cols="30" rows="10"></textarea>
+            </div>
+            <input class="btn btn-success" type="submit" name="">
+        </form>
+    </div>
 <?php require_once __DIR__. "/layouts/footer.php";  ?>
